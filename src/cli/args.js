65338b1d2ec3e990b00dd25ecdafd2c2106c9a64
andrei.k <andrei.kav9307@gmail.com>
2024-01-27 18:31:35 +0300

    cli, envs and args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    const prefix = '--'
+    const args = process.argv
+        .filter(arg => arg.startsWith(prefix))
+        .map(value => `${value.slice(prefix.length)} is ${process.argv[process.argv.indexOf(value) + 1]}`)
+    console.log(args.join(', '))
 };
 
 parseArgs();
