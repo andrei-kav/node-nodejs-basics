a8196a5212cb58677eec1983b9102236d1cbe011
andrei.k <andrei.kav9307@gmail.com>
2024-01-28 18:01:09 +0300

    some minor remarks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -2,16 +2,16 @@ import {Transform} from 'stream'
 
 const transform = async () => {
 
-    const transform = new Transform({
+    const reverse = new Transform({
         transform(chunk, encoding, callback) {
             this.push(chunk.reverse())
             callback()
         }
     })
 
-    // revert process.stdin data and write it into process.stdout
+    // reverse process.stdin data and write it into process.stdout
     process.stdin
-        .pipe(transform)
+        .pipe(reverse)
         .pipe(process.stdout)
 };
 
