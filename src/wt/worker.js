eadab62d3f08ad2945be6203c7814e60bb933af3
andrei.k <andrei.kav9307@gmail.com>
2024-01-29 18:24:19 +0300

    worker threads package

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
+import {isMainThread, parentPort, workerData} from 'worker_threads'
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    if (!isMainThread) {
+        // get data from the main thread
+        // do calculation and send back to the main thread
+        const {number} = workerData
+        const result = nthFibonacci(number)
+        parentPort.postMessage(result)
+    } else {
+        console.log('worker.js, not main thread')
+    }
 };
 
 sendResult();
