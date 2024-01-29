df0a6329ebbb8d7f2d860636575de85c17cc122d
andrei.k <andrei.kav9307@gmail.com>
2024-01-30 00:42:28 +0300

    child process part

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import {fork} from 'child_process'
+import {getPaths} from "../helpers/get-paths.js"
+import path from "path"
+
+const {__dirname} = getPaths(import.meta.url)
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childPath = path.join(__dirname, 'files', 'script.js')
+
+    const child = fork(childPath, args, { stdio: ['inherit', 'pipe', 'inherit', 'ipc'] })
+
+    child.stdout.on('data', (message) => {
+        console.log('got from child\n', message.toString())
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(['a', 'b']);
