66090cf2e938f679d36ddad35c54d48ee67eb4b3
andrei.k <andrei.kav9307@gmail.com>
2024-01-29 16:18:10 +0300

    zlib package

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import {getPaths} from "../helpers/get-paths.js";
+import path from "path";
+import fs from "fs";
+import zlib from "zlib";
+
+const {__dirname} = getPaths(import.meta.url)
+
 const compress = async () => {
-    // Write your code here 
+    const toReadPath = path.join(__dirname, 'files', 'fileToCompress.txt')
+    const readStream = fs.createReadStream(toReadPath)
+    const toWritePath = path.join(__dirname, 'files', 'archive.gz')
+    const writeStream = fs.createWriteStream(toWritePath)
+    const gzip = zlib.createGzip()
+    // compress the txt file and write it into archive.gz
+    readStream.pipe(gzip).pipe(writeStream)
 };
 
 await compress();
